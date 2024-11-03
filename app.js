813112d51f64110f8dc2861a6d39f3ae3f453143
drugos624 <69643151+drugos624@users.noreply.github.com>
2024-11-03 13:02:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
+// Importa le funzioni di cui hai bisogno dai SDK
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
 // Configura il tuo progetto Firebase
 const firebaseConfig = {
- apiKey: "AIzaSyAlu5Q1Ww3u2Dt_INeKOm5GSeEU-qFDtJs",
+    apiKey: "AIzaSyAlu5Q1Ww3u2Dt_INeKOm5GSeEU-qFDtJs",
     authDomain: "addolcitori-2f8fe.firebaseapp.com",
     databaseURL: "https://addolcitori-2f8fe-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "addolcitori-2f8fe",
-    storageBucket: "addolcitori-2f8fe.firebasestorage.app",
+    storageBucket: "addolcitori-2f8fe.appspot.com",
     messagingSenderId: "399702428879",
     appId: "1:399702428879:web:4bee57dcdc3b80eea8bbba"
 };
 
 // Inizializza Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Inizializza Firestore
 
+// Aggiungi un listener all'evento submit del modulo
 document.getElementById('dataForm').addEventListener('submit', async (e) => {
     e.preventDefault();
     const modello = document.getElementById('modello').value;
     const dimensione = document.getElementById('dimensione').value;
 
     try {
-        await db.collection('modelli').add({
+        // Aggiungi i dati a Firestore
+        await addDoc(collection(db, 'modelli'), {
             modello: modello,
             dimensione: dimensione
         });
@@ -30,3 +36,4 @@ document.getElementById('dataForm').addEventListener('submit', async (e) => {
         document.getElementById('message').innerText = 'Errore durante l\'inserimento dei dati.';
     }
 });
+
